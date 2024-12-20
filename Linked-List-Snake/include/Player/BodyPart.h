10984abb5676cf9829271e6a1a60a02e5ddf7f45
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-12-20 08:32:50 +0530

    Properties for Body part

---
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -0,0 +1,18 @@
+#pragma once
+#include"UI/UIElement/ImageView.h"
+#include"Player/Direction.h"
+
+namespace Player
+{
+	class BodyPart
+	{
+	protected:
+		UI::UIElement::ImageView* bodypart_image;
+
+		sf::Vector2i grid_position;
+		Direction direction;
+
+		float bodypart_width;
+		float bodypart_height;
+	};
+}
