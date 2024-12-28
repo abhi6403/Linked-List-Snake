ecce2fbbdaf1034736ff5f4636a2789d658b27cd
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-12-28 11:30:38 +0530

    Inserting at the Tail

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -57,13 +57,14 @@ namespace LinkedList
 
 	void SingleLinkedList::insertNodeAtTail() 
 	{
+		linked_list_size++;
 		Node* new_node = createNode();
 		Node* cur_node = head_node;
 
 		if (cur_node == nullptr) 
 		{     
 			head_node = new_node;
-			new_node->body_part.initialize(node_width, node_height, default_position, default_direction);
+			initializeNode(new_node, nullptr, Operation::TAIL);
 			return;
 		}
 
@@ -73,7 +74,7 @@ namespace LinkedList
 		}
 
 		cur_node->next = new_node;
-		new_node->body_part.initialize(node_width, node_height, getNewNodePosition(cur_node), cur_node->body_part.getDirection());
+		initializeNode(new_node, cur_node, Operation::TAIL);
 	}
 
 	void SingleLinkedList::insertNodeAtHead()
